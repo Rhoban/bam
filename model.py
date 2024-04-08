5577e25251fe7badd8a7b9cd3666d9a0cabf8218
Grégoire Passault <g.passault@gmail.com>
2024-04-08 23:53:55 +0200

    Model and simulator

---
+++ model.py
@@ -0,0 +1,120 @@
+import numpy as np
+
+
+class Parameter:
+    def __init__(self, value: float, min: float, max: float, optimize: bool = True):
+        # Current value of the parameter
+        self.value: float = value
+        # Minimum and maximum values for the parameter
+        self.min: float = min
+        self.max: float = max
+        # Should this parameter be optimized?
+        self.optimize: bool = optimize
+
+
+class BaseModel:
+    def compute_motor_torque(self, volts: float | None, dtheta: float) -> float:
+        """
+        This computes the torque applied by the motor, the friction torque and returns
+        an expected angular acceleration.
+        """
+        raise NotImplementedError
+
+    def compute_friction_torque(
+        self, motor_torque: float, external_torque: float, dtheta: float
+    ) -> float:
+        """
+        This computes the friction torque applied by the system.
+        """
+        raise NotImplementedError
+
+    def get_extra_inertia(self) -> float:
+        """
+        This returns the extra inertia of the system.
+        """
+        return 0.0
+
+    def get_parameters(self) -> list:
+        """
+        This returns the list of parameters that can be optimized.
+        """
+        return {
+            name: param
+            for name, param in vars(self).items()
+            if isinstance(param, Parameter)
+        }
+
+
+class Model(BaseModel):
+    def __init__(self):
+        # Torque constant [Nm/A] or [V/(rad/s)]
+        self.kt = Parameter(1.6, 0.1, 10.0)
+
+        # Motor resistance [Ohm]
+        self.R = Parameter(2.0, 0.1, 10.0)
+
+        # Motor armature [kg m^2]
+        self.armature = Parameter(0.005, 0.001, 0.1)
+
+        # Base friction is always here, stribeck friction is added when not moving [Nm]
+        self.friction_base = Parameter(0.05, 0.01, 1.0)
+        self.friction_stribeck = Parameter(0.1, 0.01, 1.0)
+
+        # Load-dependent friction, again base is always here and stribeck is added when not moving [Nm]
+        self.load_friction_base = Parameter(0.05, 0.01, 5.0)
+        self.load_friction_stribeck = Parameter(0.1, 0.01, 5.0)
+
+        # Stribeck velocity [rad/s] and curvature
+        self.dtheta_stribeck = Parameter(2.0, 0.1, 10.0)
+        self.alpha = Parameter(1.0, 0.5, 2.0)
+
+        # Viscous friction [Nm/(rad/s)]
+        self.friction_viscous = Parameter(0.1, 0.01, 5.0)
+
+    def compute_motor_torque(self, volts: float | None, dtheta: float) -> float:
+        # Volts to None means that the motor is disconnected
+        if volts is None:
+            return 0.0
+
+        # Torque produced
+        torque = self.kt.value * volts / self.R.value
+
+        # Back EMF
+        torque -= (self.kt.value**2) * dtheta / self.R.value
+
+        return torque
+
+    def compute_friction_torque(
+        self, motor_torque: float, external_torque: float, dtheta: float
+    ) -> float:
+        # Torque applied to the gearbox
+        gearbox_torque = np.abs(external_torque - motor_torque)
+
+        # Stribeck coeff (1 when stopped to 0 when moving)
+        stribeck_coeff = np.exp(
+            -np.abs(dtheta / self.dtheta_stribeck.value) ** self.alpha.value
+        )
+
+        # Static friction
+        static_friction = (
+            self.friction_base.value + self.load_friction_base.value * gearbox_torque
+        )
+        static_friction += stribeck_coeff * (
+            self.friction_stribeck.value
+            + self.load_friction_stribeck.value * gearbox_torque
+        )
+        net_torque = motor_torque + external_torque
+
+        # Adjust the static friction to compensate for the net torque
+        if net_torque > 0:
+            static_friction = -min(static_friction, net_torque)
+        else:
+            static_friction = -max(-static_friction, net_torque)
+
+        # Viscous friction
+        damping_friction = -self.friction_viscous.value * dtheta
+
+        return damping_friction + static_friction
+
+    def get_extra_inertia(self) -> float:
+        return self.armature.value
---
+++ simulate.py
@@ -0,0 +1,80 @@
+import numpy as np
+from model import BaseModel, Model
+
+g: float = 9.81
+
+
+class Simulate1R:
+    def __init__(self, mass: float, length: float, model: BaseModel):
+        # Mass [kg]
+        self.mass: float = mass
+        # Length [m]
+        self.length: float = length
+
+        self.screen = None
+        self.model = model
+        self.reset()
+
+    def reset(self, q: float = 0.0, dq: float = 0.0):
+        """
+        Resets the simulation to a given state
+        """
+        self.q = q
+        self.dq = dq
+        self.t = 0.0
+
+        self.inertia = self.mass * self.length**2
+
+    def step(self, volts: None | float, dt: float):
+        """
+        Steps the simulation for dt given the applied voltage.
+        """
+        gravity_torque = -self.mass * g * self.length * np.cos(self.q)
+        motor_torque = self.model.compute_motor_torque(volts, self.dq)
+        friction_torque = self.model.compute_friction_torque(
+            motor_torque, gravity_torque, self.dq
+        )
+        inertia = self.inertia + self.model.get_extra_inertia()
+        net_torque = motor_torque + friction_torque + gravity_torque
+        angular_acceleration = net_torque / inertia
+
+        self.dq += angular_acceleration * dt
+        self.q += self.dq * dt
+        self.t += dt
+
+    def draw(self):
+        """
+        Draw using pygame
+        """
+        import pygame
+
+        if self.screen is None:
+            pygame.init()
+            self.screen = pygame.display.set_mode((800, 600))
+
+        # Draw the background
+        self.screen.fill((255, 255, 255))
+        
+        # Draw the pendulum
+        x = 400 + 1000*self.length * np.cos(self.q)
+        y = 300 - 1000*self.length * np.sin(self.q)
+        pygame.draw.line(self.screen, (0, 0, 255), (400, 300), (x, y), 5)
+        pygame.draw.circle(self.screen, (255, 0, 0), (int(x), int(y)), 10)
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    import pygame
+    model = Model()
+    sim = Simulate1R(1.0, 0.15, model)
+    sim.reset(-0.*np.pi/2, 0.)
+    while True:
+        sim.step(0 if sim.t < 0.5 else None, 0.01)
+        sim.draw()
+        import time
+        time.sleep(0.01)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                break
+
