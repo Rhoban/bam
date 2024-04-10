f8b71b510589cd7c8c546e0796c3d6078717b6d7
Grégoire Passault <g.passault@gmail.com>
2024-04-10 11:20:11 +0200

    Recording, trajectory

--- record.py
+++ record.py
@@ -2,76 +2,54 @@ import json
 import os
 import numpy as np
 import argparse
-from dynamixel_sdk import *
 import time
+from dynamixel import DynamixelActuatorV1
+from trajectory import *
 
 arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument("--mass", type=float, required=True)
 arg_parser.add_argument("--length", type=float, required=True)
-arg_parser.add_argument("--duration", type=float, default=3.0)
 arg_parser.add_argument("--dt", type=float, default=0.002)
+arg_parser.add_argument("--port", type=str, default="/dev/ttyUSB0")
+arg_parser.add_argument("--trajectory", type=str, default="lift_and_drop")
 args = arg_parser.parse_args()
 
-os.system("setserial /dev/ttyUSB0 low_latency")
+if args.trajectory not in trajectories:
+    raise ValueError(f"Unknown trajectory: {args.trajectory}")
 
-portHandler = PortHandler("/dev/ttyUSB0")
-packetHandler = PacketHandler(1.0)
+dxl = DynamixelActuatorV1(args.port)
+trajectory = trajectories[args.trajectory]()
 
-portHandler.openPort()
-portHandler.setBaudRate(1000000)
+start = time.time()
+while time.time() - start < 1.0:
+    goal_position, torque_enable = trajectory(0)
+    dxl.set_goal_position(goal_position)
+    dxl.set_torque(torque_enable)
 
-def read_data():
-    # Reading position, speed, load, voltage and temperature
-    data, result, error = packetHandler.readTxRx(portHandler, 1, ADDR_READ, 8)
-
-    # Position is a 12-bit value
-    position = (data[1] << 8) | data[0]
-    position = 2 * np.pi * ((position / 4096) - 0.5)
-
-    # Speed is a 10-bit value, units are 0.11 rpm per step
-    speed = (data[3] << 8) | data[2]
-    if speed > 1024:
-        speed = -(speed - 1024)
-    speed = speed * 0.11 * 2 * np.pi / 60.0
-
-    # Applied "load"
-    load = (data[5] << 8) | data[4]
-    if load > 1024:
-        load = -(load - 1024)
-
-    # Voltage is a byte value, units are 0.1 V
-    volts = data[6] / 10.0
-
-    # Temperature are °C
-    temp = data[7]
-
-    return {
-        "position": position,
-        "speed": speed,
-        "load": load,
-        "volts": volts,
-        "temp": temp,
-    }
-
-
-ADDR_READ = 36
 start = time.time()
 data = {"mass": args.mass, "length": args.length, "dt": args.dt, "entries": []}
 
-for step in range(int(args.duration / args.dt)):
+for step in range(int(trajectory.duration / args.dt)):
     step_t = args.dt * step
     while (time.time() - start) < step_t:
         time.sleep(0.001)
 
+    goal_position, new_torque_enable = trajectory(step_t)
+    if new_torque_enable != torque_enable:
+        print("Set_torque", new_torque_enable)
+        dxl.set_torque(new_torque_enable)
+        torque_enable = new_torque_enable
+    if torque_enable:
+        dxl.set_goal_position(goal_position)
+
     t0 = time.time() - start
-    entry = read_data()
+    entry = dxl.read_data()
     t1 = time.time() - start
 
-    print((t1-t0))
-    entry["timestamp"] = (t0 + t1) /2.0
+    entry["timestamp"] = (t0 + t1) / 2.0
 
-    entry["volts"] = 0.0
-    entry["torque_enable"] = True
+    entry["goal_position"] = goal_position
+    entry["torque_enable"] = torque_enable
     data["entries"].append(entry)
 
 json.dump(data, open("data.json", "w"))
---
+++ trajectory.py
@@ -0,0 +1,66 @@
+import numpy as np
+
+
+def cubic_interpolate(keyframes: list, t: float):
+    if t < keyframes[0][0]:
+        return keyframes[0][1]
+    if t > keyframes[-1][0]:
+        return keyframes[-1][1]
+
+    for i in range(len(keyframes) - 1):
+        if keyframes[i][0] <= t <= keyframes[i + 1][0]:
+            t0, x0, x0p = keyframes[i]
+            t1, x1, x1p = keyframes[i + 1]
+
+            A = [
+                [1, t0, t0**2, t0**3],
+                [0, 1, 2 * t0, 3 * t0**2],
+                [1, t1, t1**2, t1**3],
+                [0, 1, 2 * t1, 3 * t1**2],
+            ]
+            b = [x0, x0p, x1, x1p]
+            w = np.linalg.solve(A, b)
+
+            return w[0] + w[1] * t + w[2] * t**2 + w[3] * t**3
+
+
+class Trajectory:
+    duration = None
+
+    def __call__(self, t: float):
+        """
+        Retrieve (angle, torque_enable) at time t
+        """
+        raise NotImplementedError
+
+class LiftAndDrop(Trajectory):
+    duration = 5.0
+
+    def __call__(self, t: float):
+        keyframes = [[0.0, 0.0, 0.0], [2.0, np.pi / 2, 0.0]]
+        angle = cubic_interpolate(keyframes, t)
+
+        enable = t < 2.0
+
+        return angle, enable
+    
+trajectories = {
+    "lift_and_drop": LiftAndDrop
+}
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+
+    trajectory = LiftAndDrop()
+
+    ts = np.linspace(0.0, 5.0, 1000)
+    xs = [trajectory(t)[0] for t in ts]
+    en = [trajectory(t)[1] for t in ts]
+
+    plt.plot(ts, xs, label="Angle")
+    plt.fill_between(ts, min(xs), max(xs), en, alpha=0.2, color="green", label="Torque enable")
+    plt.xlabel("Time (s)")
+    plt.ylabel("Angle (rad)")
+    plt.legend()
+    plt.grid()
+    plt.show()
