c705716eb65f3a21d5b19f301a1ffc2f02e00b21
Gregwar <g.passault@gmail.com>
2024-04-13 12:01:26 +0200

    Passing only exp_id

--- run.sh
+++ run.sh
@@ -1,17 +1,16 @@
 
-MODEL="stribeck_viscous"
+#MODEL="m1"
 DATA="data_106"
 
-for k in `seq 1 1`
+for MODEL in m1 m2 m3 m4:
 do
 	N=`ls output/*.json|wc -w`
 	N=$[$N + 1]
-	JSON="output/params_$N.json"
-	touch $JSON
+	
 	python fit.py --wandb --logdir $DATA \
 		--control --model $MODEL \
 		--workers 1 --trials 50000 \
-		--output $JSON \
+		--output $N \
 		> output/fit_$N.log 2>&1
 	echo $k
 done
