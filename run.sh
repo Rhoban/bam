7662dc12bce9b30fbb6d5f59b2add29de487eb7f
Gregwar <g.passault@gmail.com>
2024-04-18 21:01:23 +0200

    Detail un run.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,15 @@
 
 #MODEL="m1"
 DATA="data_106"
+ARGS="--control"
 
-for MODEL in m1 m2 m3 m4:
+for MODEL in m1
 do
 	N=`ls output/*.json|wc -w`
 	N=$[$N + 1]
 	
 	python fit.py --wandb --logdir $DATA \
-		--control --model $MODEL \
+		$ARGS --model $MODEL \
 		--workers 1 --trials 10000 \
 		--output $N \
 		> output/fit_$N.log 2>&1
