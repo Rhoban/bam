f671553ebce88d8634c348210f7c474f82c2f658
Grégoire Passault <g.passault@gmail.com>
2024-04-18 13:40:48 +0200

    Multi-logs plot

--- plot.py
+++ plot.py
@@ -19,13 +19,21 @@ args = arg_parser.parse_args()
 logs = logs.Logs(args.logdir)
 
 if args.sim:
-    model = load_model(args.params)
+    model_names = args.params.split(",")
 
 for log in logs.logs:
+    all_sim_q = []
+    all_sim_volts = []
+    all_names = []
+
     if args.sim:
-        simulator = simulate.Simulate1R(log["mass"], log["length"], model)
-        sim_q, sim_volts = simulator.rollout_log(log, reset_period=args.reset_period, simulate_control=args.control)
-        sim_q = np.array(sim_q)
+        for model_name in model_names:
+            model = load_model(model_name)
+            all_names.append(model.name)
+            simulator = simulate.Simulate1R(log["mass"], log["length"], model)
+            sim_q, sim_volts = simulator.rollout_log(log, reset_period=args.reset_period, simulate_control=args.control)
+            all_sim_q.append(np.array(sim_q))
+            all_sim_volts.append(np.array(sim_volts))
 
     ts = np.arange(len(log["entries"])) * log["dt"]
     q = [entry["position"] for entry in log["entries"]]
@@ -37,13 +45,13 @@ for log in logs.logs:
     f, (ax1, ax2) = plt.subplots(2, sharex=True)
 
     ax1.plot(ts, q, label="q")
+    ax1.plot(ts, goal_q, label="goal_q", color="black", linestyle="--")
     if args.sim:
-        ax1.plot(ts, sim_q, label="sim_q")
-    ax1.plot(ts, goal_q, label="goal_q", color="green")
+        for model_name, sim_q in zip(all_names, all_sim_q):
+            ax1.plot(ts, sim_q, label=f"{model_name}_q")
     ax1.legend()
     title = f'{log["motor"]}, {log["trajectory"]}, m={log["mass"]}, l={log["length"]}, k={log["kp"]}'
-    if args.sim:
-        title = model.name+", "+title
+
     ax1.set_title(f'{log["motor"]}, {log["trajectory"]}, m={log["mass"]}, l={log["length"]}, k={log["kp"]}')
     ax1.grid()
 
@@ -51,7 +59,8 @@ for log in logs.logs:
     ax2.plot(ts, volts, label="volts")
     if args.control:
         if args.sim:
-            ax2.plot(ts, sim_volts, label="sim_volts")
+            for model_name, sim_volts in zip(all_names, all_sim_volts):
+                ax2.plot(ts, sim_volts, label=f"{model_name}_volts")
     # Shading the areas where torque is False
     ax2.fill_between(
         ts,
