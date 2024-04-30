a46b2c0ced1343812d331748d042a56d1a6693a0
Grégoire Passault <g.passault@gmail.com>
2024-04-30 23:32:13 +0200

    Parameter definition

---
+++ parameter.py
@@ -0,0 +1,12 @@
+
+class Parameter:
+    def __init__(self, value: float, min: float, max: float, optimize: bool = True):
+        # Current value of the parameter
+        self.value: float = value
+
+        # Minimum and maximum values for the parameter
+        self.min: float = min
+        self.max: float = max
+
+        # Should this parameter be optimized?
+        self.optimize: bool = optimize
