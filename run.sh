1576400c06d2aa272497f35562c8cbc58263c0fa
Gregwar <g.passault@gmail.com>
2024-04-30 23:44:29 +0200

    Updating ruh.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 DATA="data_106"
 ARGS="--control"
 
-for MODEL in m1
+for MODEL in m1 m2 m3 m4
 do
 	N=`ls output/*.json|wc -w`
 	N=$[$N + 1]
@@ -11,6 +11,7 @@ do
 	python fit.py --wandb --logdir $DATA \
 		$ARGS --model $MODEL \
 		--workers 1 --trials 10000 \
+		--actuator mx \
 		--output $N \
 		> output/fit_$N.log 2>&1
 	echo $k
