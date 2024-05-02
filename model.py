647eeb7498fc31b103347f5fda56c34ffa44a912
Grégoire Passault <g.passault@gmail.com>
2024-05-02 08:57:25 +0200

    Revert "Fixing stribeck curvature"
    This reverts commit 61096461c76f0c73a3082f47ac833df5ef681fca.

--- model.py
+++ model.py
@@ -144,9 +144,8 @@ class Model(BaseModel):
                     - motor_torque * self.load_friction_motor_stribeck.value
                 )
                 if self.load_curvature:
-                    gearbox_torque_stribeck = (
-                        gearbox_torque_stribeck
-                        ** self.load_friction_curvature_stribeck.value
+                    gearbox_torque = (
+                        gearbox_torque**self.load_friction_curvature_stribeck.value
                     )
 
         else:
@@ -199,11 +198,7 @@ models = {
         name="m5", load_dependent=True, directional=True, stribeck=True
     ),
     "m6": lambda: Model(
-        name="m6",
-        load_dependent=True,
-        directional=True,
-        stribeck=True,
-        load_curvature=True,
+        name="m6", load_dependent=True, directional=True, stribeck=True, load_curvature=True
     ),
 }
 
