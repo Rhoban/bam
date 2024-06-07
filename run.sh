8642e1ef6b858514af8acbc4893b4c98cb2e0c13
Gregoire Passault <gpassault@emi.u-bordeaux.fr>
2024-06-07 23:37:22 +0200

    Adjusting run.sh

--- run.sh
+++ run.sh
@@ -1,20 +1,18 @@
 
-#MODEL="m1"
+MODEL=m1
 DATA="data_106"
 ARGS="--control"
+VAL_KP=8
 
-for MODEL in m1 m2 m3 m4
-do
-	N=`ls output/*.json|wc -w`
-	N=$[$N + 1]
-	
-	python fit.py --wandb --logdir $DATA \
-		$ARGS --model $MODEL \
-		--workers 1 --trials 10000 \
-		--actuator mx \
-		--output $N \
-		> output/fit_$N.log 2>&1
-	echo $k
-done
+N=`ls output/*.json|wc -w`
+N=$[$N + 1]
+
+python fit.py --wandb --logdir $DATA \
+	$ARGS --model $MODEL \
+	--workers 1 --trials 10000 \
+	--validation_kp $VAL_KP \
+	--actuator mx \
+	--output $N \
+	> output/fit_$N.log 2>&1
 
 
