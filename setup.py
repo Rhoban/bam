3d35fdec028cfb8d9e95195b68687bcc2499f3d6
Grégoire Passault <g.passault@gmail.com>
2024-06-11 14:51:42 +0200

    Rham

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 import setuptools
 
 setuptools.setup(
-    name="bam",
+    name="rham",
     version="0.0.1",
     author="Rhoban team",
     author_email="team@rhoban.com",
     description="Work in progress",
     long_description="",
     long_description_content_type="text/markdown",
-    url="https://github.com/rhoban/bam/",
+    url="https://github.com/rhoban/rham/",
     packages=setuptools.find_packages(),
     entry_points={},
     classifiers=[
